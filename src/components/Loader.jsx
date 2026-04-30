e4200facc04b23a78636aba54aced1ae0f80bb85
kaushinder <kaushindersinghraghav@gmail.com>
2026-04-30 13:34:03 +0530

    Refactor Loader component to implement a new EVA animation design

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,53 +4,22 @@ import styled from 'styled-components';
 const Loader = () => {
   return (
     <StyledWrapper>
-      <div>
-        <div className="box-of-star1">
-          <div className="star star-position1" />
-          <div className="star star-position2" />
-          <div className="star star-position3" />
-          <div className="star star-position4" />
-          <div className="star star-position5" />
-          <div className="star star-position6" />
-          <div className="star star-position7" />
-        </div>
-        <div className="box-of-star2">
-          <div className="star star-position1" />
-          <div className="star star-position2" />
-          <div className="star star-position3" />
-          <div className="star star-position4" />
-          <div className="star star-position5" />
-          <div className="star star-position6" />
-          <div className="star star-position7" />
-        </div>
-        <div className="box-of-star3">
-          <div className="star star-position1" />
-          <div className="star star-position2" />
-          <div className="star star-position3" />
-          <div className="star star-position4" />
-          <div className="star star-position5" />
-          <div className="star star-position6" />
-          <div className="star star-position7" />
-        </div>
-        <div className="box-of-star4">
-          <div className="star star-position1" />
-          <div className="star star-position2" />
-          <div className="star star-position3" />
-          <div className="star star-position4" />
-          <div className="star star-position5" />
-          <div className="star star-position6" />
-          <div className="star star-position7" />
-        </div>
-        <div data-js="astro" className="astronaut">
-          <div className="head" />
-          <div className="arm arm-left" />
-          <div className="arm arm-right" />
-          <div className="body">
-            <div className="panel" />
+      <div className="loader">
+        <div className="modelViewPort">
+          <div className="eva">
+            <div className="head">
+              <div className="eyeChamber">
+                <div className="eye" />
+                <div className="eye" />
+              </div>
+            </div>
+            <div className="body">
+              <div className="hand" />
+              <div className="hand" />
+              <div className="scannerThing" />
+              <div className="scannerOrigin" />
+            </div>
           </div>
-          <div className="leg leg-left" />
-          <div className="leg leg-right" />
-          <div className="schoolbag" />
         </div>
       </div>
     </StyledWrapper>
@@ -58,332 +27,174 @@ const Loader = () => {
 }
 
 const StyledWrapper = styled.div`
-  @keyframes snow {
-    0% {
-      opacity: 0;
-      transform: translateY(0px);
+  .modelViewPort {
+    /* The black circle background around the model*/
+    perspective: 1000px;
+    width: 20rem;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #000;
+    overflow: hidden;
+  }
+  .eva {
+    --EVA-ROTATION-DURATION: 4s;
+    transform-style: preserve-3d;
+    animation: rotateRight var(--EVA-ROTATION-DURATION) linear infinite alternate;
+  }
+  .head {
+    position: relative;
+    width: 6rem;
+    height: 4rem;
+    border-radius: 48% 53% 45% 55% / 79% 79% 20% 22%;
+    background: linear-gradient(to right, white 45%, gray);
+  }
+  .eyeChamber {
+    width: 4.5rem;
+    height: 2.75rem;
+    position: relative;
+    left: 50%;
+    top: 55%;
+    border-radius: 45% 53% 45% 48% / 62% 59% 35% 34%;
+    background-color: #0c203c;
+    box-shadow: 0px 0px 2px 2px white, inset 0px 0px 0px 2px black;
+    transform: translate(-50%, -50%);
+    animation: moveRight var(--EVA-ROTATION-DURATION) linear infinite alternate;
+  }
+  .eye {
+    width: 1.2rem;
+    height: 1.5rem;
+    position: absolute;
+    border-radius: 50%;
+  }
+  .eye:first-child {
+    left: 12px;
+    top: 50%;
+    background: repeating-linear-gradient(
+      65deg,
+      #9bdaeb 0px,
+      #9bdaeb 1px,
+      white 2px
+    );
+    box-shadow: inset 0px 0px 5px #04b8d5, 0px 0px 15px 1px #0bdaeb;
+    transform: translate(0, -50%) rotate(-65deg);
+  }
+  .eye:nth-child(2) {
+    right: 12px;
+    top: 50%;
+    background: repeating-linear-gradient(
+      -65deg,
+      #9bdaeb 0px,
+      #9bdaeb 1px,
+      white 2px
+    );
+    box-shadow: inset 0px 0px 5px #04b8d5, 0px 0px 15px 1px #0bdaeb;
+    transform: translate(0, -50%) rotate(65deg);
+  }
+  .body {
+    width: 6rem;
+    height: 8rem;
+    position: relative;
+    margin-block-start: 0.25rem;
+    border-radius: 47% 53% 45% 55% / 12% 9% 90% 88%;
+    background: linear-gradient(to right, white 35%, gray);
+  }
+  .hand {
+    position: absolute;
+    left: -1.5rem;
+    top: 0.75rem;
+    width: 2rem;
+    height: 5.5rem;
+    border-radius: 40%;
+    background: linear-gradient(to left, white 15%, gray);
+    box-shadow: 5px 0px 5px rgba(0, 0, 0, 0.25);
+    transform: rotateY(55deg) rotateZ(10deg);
+  }
+  .hand:first-child {
+    animation: compensateRotation var(--EVA-ROTATION-DURATION) linear infinite
+      alternate;
+  }
+  .hand:nth-child(2) {
+    left: 92%;
+    background: linear-gradient(to right, white 15%, gray);
+    transform: rotateY(55deg) rotateZ(-10deg);
+    animation: compensateRotationRight var(--EVA-ROTATION-DURATION) linear
+      infinite alternate;
+  }
+  .scannerThing {
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 60%;
+    top: 10%;
+    border-top: 180px solid #9bdaeb;
+    border-left: 250px solid transparent;
+    border-right: 250px solid transparent;
+    transform-origin: top left;
+    mask: linear-gradient(to right, white, transparent 35%);
+    animation: glow 2s cubic-bezier(0.86, 0, 0.07, 1) infinite;
+  }
+  .scannerOrigin {
+    position: absolute;
+    width: 8px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    left: 60%;
+    top: 10%;
+    background: #9bdaeb;
+    box-shadow: inset 0px 0px 5px rgba(0, 0, 0, 0.5);
+    animation: moveRight var(--EVA-ROTATION-DURATION) linear infinite;
+  }
+  @keyframes rotateRight {
+    from {
+      transform: rotateY(0deg);
     }
-
+    to {
+      transform: rotateY(25deg);
+    }
+  }
+  @keyframes moveRight {
+    from {
+      transform: translate(-50%, -50%);
+    }
+    to {
+      transform: translate(-40%, -50%);
+    }
+  }
+  @keyframes compensateRotation {
+    from {
+      transform: rotateY(55deg) rotateZ(10deg);
+    }
+    to {
+      transform: rotatey(30deg) rotateZ(10deg);
+    }
+  }
+  @keyframes compensateRotationRight {
+    from {
+      transform: rotateY(55deg) rotateZ(-10deg);
+    }
+    to {
+      transform: rotateY(70deg) rotateZ(-10deg);
+    }
+  }
+  @keyframes glow {
+    from {
+      opacity: 0;
+    }
     20% {
       opacity: 1;
     }
-
+    45% {
+      transform: rotate(-25deg);
+    }
+    75% {
+      transform: rotate(5deg);
+    }
     100% {
-      opacity: 1;
-      transform: translateY(650px);
+      opacity: 0;
     }
-  }
-
-  @keyframes astronaut {
-    0% {
-      transform: rotate(0deg);
-    }
-
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-
-  .box-of-star1,
-  .box-of-star2,
-  .box-of-star3,
-  .box-of-star4 {
-    width: 100%;
-    position: absolute;
-    z-index: 10;
-    left: 0;
-    top: 0;
-    transform: translateY(0px);
-    height: 700px;
-  }
-
-  .box-of-star1 {
-    animation: snow 5s linear infinite;
-  }
-
-  .box-of-star2 {
-    animation: snow 5s -1.64s linear infinite;
-  }
-
-  .box-of-star3 {
-    animation: snow 5s -2.30s linear infinite;
-  }
-
-  .box-of-star4 {
-    animation: snow 5s -3.30s linear infinite;
-  }
-
-  .star {
-    width: 3px;
-    height: 3px;
-    border-radius: 50%;
-    background-color: #FFF;
-    position: absolute;
-    z-index: 10;
-    opacity: 0.7;
-  }
-
-  .star:before {
-    content: "";
-    width: 6px;
-    height: 6px;
-    border-radius: 50%;
-    background-color: #FFF;
-    position: absolute;
-    z-index: 10;
-    top: 80px;
-    left: 70px;
-    opacity: .7;
-  }
-
-  .star:after {
-    content: "";
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background-color: #FFF;
-    position: absolute;
-    z-index: 10;
-    top: 8px;
-    left: 170px;
-    opacity: .9;
-  }
-
-  .star-position1 {
-    top: 30px;
-    left: 20px;
-  }
-
-  .star-position2 {
-    top: 110px;
-    left: 250px;
-  }
-
-  .star-position3 {
-    top: 60px;
-    left: 570px;
-  }
-
-  .star-position4 {
-    top: 120px;
-    left: 900px;
-  }
-
-  .star-position5 {
-    top: 20px;
-    left: 1120px;
-  }
-
-  .star-position6 {
-    top: 90px;
-    left: 1280px;
-  }
-
-  .star-position7 {
-    top: 30px;
-    left: 1480px;
-  }
-
-  .astronaut {
-    width: 250px;
-    height: 300px;
-    position: absolute;
-    z-index: 11;
-    top: calc(50% - 150px);
-    left: calc(50% - 125px);
-    animation: astronaut 5s linear infinite;
-  }
-
-  .schoolbag {
-    width: 100px;
-    height: 150px;
-    position: absolute;
-    z-index: 1;
-    top: calc(50% - 75px);
-    left: calc(50% - 50px);
-    background-color: #94b7ca;
-    border-radius: 50px 50px 0 0 / 30px 30px 0 0;
-  }
-
-  .head {
-    width: 97px;
-    height: 80px;
-    position: absolute;
-    z-index: 3;
-    background: -webkit-linear-gradient(left, #e3e8eb 0%, #e3e8eb 50%, #fbfdfa 50%, #fbfdfa 100%);
-    border-radius: 50%;
-    top: 34px;
-    left: calc(50% - 47.5px);
-  }
-
-  .head:after {
-    content: "";
-    width: 60px;
-    height: 50px;
-    position: absolute;
-    top: calc(50% - 25px);
-    left: calc(50% - 30px);
-    background: -webkit-linear-gradient(top, #15aece 0%, #15aece 50%, #0391bf 50%, #0391bf 100%);
-    border-radius: 15px;
-  }
-
-  .head:before {
-    content: "";
-    width: 12px;
-    height: 25px;
-    position: absolute;
-    top: calc(50% - 12.5px);
-    left: -4px;
-    background-color: #618095;
-    border-radius: 5px;
-    box-shadow: 92px 0px 0px #618095;
-  }
-
-  .body {
-    width: 85px;
-    height: 100px;
-    position: absolute;
-    z-index: 2;
-    background-color: #fffbff;
-    border-radius: 40px / 20px;
-    top: 105px;
-    left: calc(50% - 41px);
-    background: -webkit-linear-gradient(left, #e3e8eb 0%, #e3e8eb 50%, #fbfdfa 50%, #fbfdfa 100%);
-  }
-
-  .panel {
-    width: 60px;
-    height: 40px;
-    position: absolute;
-    top: 20px;
-    left: calc(50% - 30px);
-    background-color: #b7cceb;
-  }
-
-  .panel:before {
-    content: "";
-    width: 30px;
-    height: 5px;
-    position: absolute;
-    top: 9px;
-    left: 7px;
-    background-color: #fbfdfa;
-    box-shadow: 0px 9px 0px #fbfdfa, 0px 18px 0px #fbfdfa;
-  }
-
-  .panel:after {
-    content: "";
-    width: 8px;
-    height: 8px;
-    position: absolute;
-    top: 9px;
-    right: 7px;
-    background-color: #fbfdfa;
-    border-radius: 50%;
-    box-shadow: 0px 14px 0px 2px #fbfdfa;
-  }
-
-  .arm {
-    width: 80px;
-    height: 30px;
-    position: absolute;
-    top: 121px;
-    z-index: 2;
-  }
-
-  .arm-left {
-    left: 30px;
-    background-color: #e3e8eb;
-    border-radius: 0 0 0 39px;
-  }
-
-  .arm-right {
-    right: 30px;
-    background-color: #fbfdfa;
-    border-radius: 0 0 39px 0;
-  }
-
-  .arm-left:before,
-  .arm-right:before {
-    content: "";
-    width: 30px;
-    height: 70px;
-    position: absolute;
-    top: -40px;
-  }
-
-  .arm-left:before {
-    border-radius: 50px 50px 0px 120px / 50px 50px 0 110px;
-    left: 0;
-    background-color: #e3e8eb;
-  }
-
-  .arm-right:before {
-    border-radius: 50px 50px 120px 0 / 50px 50px 110px 0;
-    right: 0;
-    background-color: #fbfdfa;
-  }
-
-  .arm-left:after,
-  .arm-right:after {
-    content: "";
-    width: 30px;
-    height: 10px;
-    position: absolute;
-    top: -24px;
-  }
-
-  .arm-left:after {
-    background-color: #6e91a4;
-    left: 0;
-  }
-
-  .arm-right:after {
-    right: 0;
-    background-color: #b6d2e0;
-  }
-
-  .leg {
-    width: 30px;
-    height: 40px;
-    position: absolute;
-    z-index: 2;
-    bottom: 70px;
-  }
-
-  .leg-left {
-    left: 76px;
-    background-color: #e3e8eb;
-    transform: rotate(20deg);
-  }
-
-  .leg-right {
-    right: 73px;
-    background-color: #fbfdfa;
-    transform: rotate(-20deg);
-  }
-
-  .leg-left:before,
-  .leg-right:before {
-    content: "";
-    width: 50px;
-    height: 25px;
-    position: absolute;
-    bottom: -26px;
-  }
-
-  .leg-left:before {
-    left: -20px;
-    background-color: #e3e8eb;
-    border-radius: 30px 0 0 0;
-    border-bottom: 10px solid #6d96ac;
-  }
-
-  .leg-right:before {
-    right: -20px;
-    background-color: #fbfdfa;
-    border-radius: 0 30px 0 0;
-    border-bottom: 10px solid #b0cfe4;
   }`;
 
 export default Loader;
